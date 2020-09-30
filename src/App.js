c288306cfd4ffad5b8d1df97b348e44ce7ea1ccf
uttkarsh <uttkarshsingh789@gmail.com>
2020-09-30 17:44:40 +0530

    made the frontend

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from "react";
+//hooks are the way of setting the state in the functional compnent
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 import Header from "./components/ui/Header";
+import CharacterGrid from "./components/characters/CharacterGrid";
 
 function App() {
+  const [items, setItems] = useState([]); //this is used for setting up the state
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    //this is used for calling and manupulating the state
+    const fetchItems = async () => {
+      const result = await axios.get(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+      console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    };
+    fetchItems();
+  }, []);
   return (
     <div className="App">
       <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
 
 export default App;
-// https://youtu.be/YaioUnMw0mo?list=PLillGF-RfqbY3c2r0htQyVbDJJoBFE6Rb&t=607
 // left the video at this time
